4f44ad944009bae95fe2b02216d4f6c3b112c8ca
daniel <daniel@192.168.178.21>
2014-05-20 14:24:23 +0200

    start with example project

---
+++ kinosim/src/main/java/de/outstare/java/kinosim/App.java
@@ -0,0 +1,13 @@
+package de.outstare.java.kinosim;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
