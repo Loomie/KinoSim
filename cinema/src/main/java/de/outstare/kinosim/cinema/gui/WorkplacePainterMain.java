cc4becb8def0ecdbb0c8b3ea192f29c2a503696f
Baret <Beret14@web.de>
2014-09-12 15:37:43 +0200

    Testwindows are now aligned next to each other

--- cinema/src/main/java/de/outstare/kinosim/cinema/gui/WorkplacePainterMain.java
+++ cinema/src/main/java/de/outstare/kinosim/cinema/gui/WorkplacePainterMain.java
@@ -1,5 +1,9 @@
 package de.outstare.kinosim.cinema.gui;
 
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
+import java.awt.Rectangle;
+
 import javax.swing.JFrame;
 
 import org.slf4j.Logger;
@@ -27,9 +31,28 @@ public class WorkplacePainterMain extends JFrame {
 	}
 
 	public static void main(final String[] args) {
-		for (int i = 500; i <= 10000; i += 500) {
+		WorkplacePainterMain last = null;
+		int maxY = 0;
+		final Rectangle bounds = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().getBounds();
+		for (int i = 500; i <= 7000; i += 500) {
+			Point pos = new Point(0, 0);
 			final WorkplacePainterMain w = new WorkplacePainterMain(i);
+			if (last != null) {
+				pos = last.getLocation();
+				if (pos.x + w.getWidth() > bounds.width) {
+					pos.x = 0;
+					pos.y += maxY;
+					maxY = 0;
+				} else {
+					pos.x += last.getWidth();
+					maxY = Math.max(maxY, pos.y + w.getHeight());
+				}
+				if (pos.y + w.getHeight() > bounds.height) {
+					pos.y = 0;
+				}
+			}
+			w.setLocation(pos);
+			last = w;
 		}
 	}
-
 }
