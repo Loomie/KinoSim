3450fd55a50eb169555990cb94b7ce6ee48eccaf
Loomie <DanielRaap@gmx.de>
2014-09-13 08:37:28 +0200

    corrected test

--- population/src/test/java/de/outstare/kinosim/population/PopulationPyramidTest.java
+++ population/src/test/java/de/outstare/kinosim/population/PopulationPyramidTest.java
@@ -5,9 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import de.outstare.kinosim.population.Audience;
-import de.outstare.kinosim.population.PopulationPyramid;
-
 public class PopulationPyramidTest {
 	private PopulationPyramid	objectUnderTest;
 
@@ -22,7 +19,7 @@ public class PopulationPyramidTest {
 		assertEquals(6720, objectUnderTest.getPopulationOfAudience(Audience.TEENS));
 		assertEquals(7500, objectUnderTest.getPopulationOfAudience(Audience.TWENS));
 		assertEquals(12000, objectUnderTest.getPopulationOfAudience(Audience.ADULTS));
-		assertEquals(10500, objectUnderTest.getPopulationOfAudience(Audience.SENIORS));
+		assertEquals(17500, objectUnderTest.getPopulationOfAudience(Audience.SENIORS));
 	}
 
 }
