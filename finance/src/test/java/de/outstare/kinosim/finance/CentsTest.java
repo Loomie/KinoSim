c6c1fc254f7605b7308ce25282576b963e3e13d8
Loomie <DanielRaap@gmx.de>
2014-10-26 09:18:26 +0100

    added tests for Cents and Leasehold

---
+++ finance/src/test/java/de/outstare/kinosim/finance/CentsTest.java
@@ -0,0 +1,117 @@
+package de.outstare.kinosim.finance;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CentsTest {
+
+	@Test
+	public void testOf() {
+		final long[] testValues = new long[] { 0L, Long.MAX_VALUE, Long.MIN_VALUE };
+
+		for (final long value : testValues) {
+			final Cents result = Cents.of(value);
+			assertEquals(value, result.getValue());
+		}
+	}
+
+	@Test
+	public void testAdd() {
+		Cents first = Cents.of(1);
+		Cents second = Cents.of(2);
+		Cents result = first.add(second);
+		assertEquals(3, result.getValue());
+
+		first = Cents.of(Integer.MAX_VALUE);
+		second = Cents.of(Integer.MAX_VALUE);
+		result = first.add(second);
+		assertEquals(2L * Integer.MAX_VALUE, result.getValue());
+
+		first = Cents.of(Long.MAX_VALUE);
+		second = Cents.of(1);
+		try {
+			result = first.add(second);
+			fail("addition overflowed long to " + result.getValue());
+		} catch (final ArithmeticException e) {
+			// expected
+		}
+	}
+
+	@Test
+	public void testSubtract() {
+		Cents first = Cents.of(2);
+		Cents second = Cents.of(1);
+		Cents result = first.subtract(second);
+		assertEquals(1, result.getValue());
+
+		first = Cents.of(Integer.MIN_VALUE);
+		second = Cents.of(Integer.MAX_VALUE);
+		result = first.subtract(second);
+		assertEquals(2L * Integer.MIN_VALUE + 1, result.getValue());
+
+		first = Cents.of(Long.MIN_VALUE);
+		second = Cents.of(1);
+		try {
+			result = first.subtract(second);
+			fail("subtract underflowed long to " + result.getValue());
+		} catch (final ArithmeticException e) {
+			// expected
+		}
+	}
+
+	@Test
+	public void testCompareTo() {
+		Cents first = Cents.of(1);
+		Cents second = Cents.of(2);
+		assertEquals(-1, first.compareTo(second));
+		assertEquals(1, second.compareTo(first));
+
+		first = Cents.of(Integer.MAX_VALUE);
+		second = Cents.of(Integer.MAX_VALUE + 1L);
+		assertEquals(-1, first.compareTo(second));
+		assertEquals(1, second.compareTo(first));
+
+		first = Cents.of(Long.MIN_VALUE);
+		second = Cents.of(Long.MAX_VALUE);
+		assertEquals(-1, first.compareTo(second));
+		assertEquals(1, second.compareTo(first));
+
+		first = Cents.of(Long.MAX_VALUE);
+		second = Cents.of(Long.MAX_VALUE);
+		assertEquals(0, first.compareTo(second));
+		assertEquals(0, second.compareTo(first));
+
+		first = Cents.of(Long.MIN_VALUE);
+		second = Cents.of(Long.MIN_VALUE);
+		assertEquals(0, first.compareTo(second));
+		assertEquals(0, second.compareTo(first));
+	}
+
+	@Test
+	public void testEqualsAndHashCode() {
+		final Cents first = Cents.of(1);
+
+		// x.equals(null) should return false
+		assertFalse(first.equals(null));
+		// must have same class
+		assertFalse(first.equals("1"));
+		assertFalse(first.equals(Long.valueOf(1)));
+		// reflexive
+		assertTrue(first.equals(first));
+
+		Cents second = Cents.of(2);
+
+		assertFalse(first.equals(second));
+		assertFalse(second.equals(first));
+		assertNotEquals(first.hashCode(), second.hashCode());
+
+		second = second.subtract(first);
+
+		// symmetric
+		assertTrue(first.equals(second));
+		assertTrue(second.equals(first));
+		assertEquals(first.hashCode(), second.hashCode());
+	}
+
+}
---
+++ finance/src/test/java/de/outstare/kinosim/finance/expenses/LeaseholdTest.java
@@ -0,0 +1,50 @@
+package de.outstare.kinosim.finance.expenses;
+
+import static org.junit.Assert.*;
+
+import java.util.Collection;
+
+import org.junit.Test;
+
+import de.outstare.kinosim.cinema.MovieTheater;
+import de.outstare.kinosim.cinema.Room;
+import de.outstare.kinosim.finance.Cents;
+
+public class LeaseholdTest {
+
+	private static class TheaterMock implements MovieTheater {
+		private final double	squareMeters;
+
+		TheaterMock(final double squareMeters) {
+			this.squareMeters = squareMeters;
+		}
+
+		@Override
+		public Collection<Room> getRooms() {
+			return null;
+		}
+
+		@Override
+		public int getNumberOfSeats() {
+			return 0;
+		}
+
+		@Override
+		public double getEstateSpace() {
+			return squareMeters;
+		}
+	}
+
+	@Test
+	public void testLeasehold() {
+		final double squareMeters = 1234.56;
+		final MovieTheater theater = new TheaterMock(squareMeters);
+		final Cents rent = Cents.of(678);
+
+		final Leasehold result = new Leasehold(theater, rent);
+
+		final Cents expected = Cents.of(Math.round(rent.getValue() * squareMeters));
+		assertEquals(expected, result.monthlyRate.amount);
+	}
+
+}
