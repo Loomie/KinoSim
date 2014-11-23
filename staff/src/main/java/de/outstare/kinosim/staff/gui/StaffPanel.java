dfc3d83605e493aec12853580912b547e5f72804
Baret <Beret14@web.de>
2014-11-23 14:22:45 +0100

    Changed StaffPanel to be a JXTitledPanel

--- staff/src/main/java/de/outstare/kinosim/staff/gui/StaffPanel.java
+++ staff/src/main/java/de/outstare/kinosim/staff/gui/StaffPanel.java
@@ -1,19 +1,17 @@
 package de.outstare.kinosim.staff.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
 
 import org.jdesktop.swingx.JXCollapsiblePane;
+import org.jdesktop.swingx.JXTitledPanel;
 import org.jdesktop.swingx.VerticalLayout;
 
 import de.outstare.kinosim.staff.Staff;
 
-public class StaffPanel extends JPanel {
+public class StaffPanel extends JXTitledPanel {
 
 	/**
 	 *
@@ -24,22 +22,9 @@ public class StaffPanel extends JPanel {
 	 * @param staff
 	 */
 	public StaffPanel(final Staff staff) {
-		super();
-		setLayout(new BorderLayout(1, 5));
-		add(new JLabel(staff.getFullName()), BorderLayout.NORTH);
-		// label.setIcon(UIManager.getIcon("Tree.expandedIcon"));
-		// label.addMouseListener(new MouseAdapter() {
-		// @Override
-		// public void mouseClicked(final MouseEvent e) {
-		// final boolean isCollapsed = collapsible.isCollapsed();
-		// collapsible.setCollapsed(!isCollapsed);
-		// final String iconKey = isCollapsed ? "Tree.expandedIcon" : "Tree.collapsedIcon";
-		// label.setIcon(UIManager.getIcon(iconKey));
-		// }
-		// });
+		super(staff.getFullName());
 		final JXCollapsiblePane collapsible = new JXCollapsiblePane();
 		final SkillSetPanel skillsPanel = new SkillSetPanel(staff.getSkills());
-		skillsPanel.setBorder(new LineBorder(Color.BLACK));
 		collapsible.add(skillsPanel);
 		final JButton toggle = new JButton(collapsible.getActionMap().get(JXCollapsiblePane.TOGGLE_ACTION));
 		toggle.setText("Show/Hide skills");
