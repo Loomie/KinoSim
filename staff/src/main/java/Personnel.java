5b6cc11b7768f74471a7318158cd8eb9f295e34e
Loomie <DanielRaap@gmx.de>
2014-11-30 15:20:24 +0100

    List of employed personell

---
+++ staff/src/main/java/Personnel.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import de.outstare.kinosim.staff.Staff;
+
+/**
+ * A Personnel is the total list of all your members
+ */
+public class Personnel implements Iterable<Staff> {
+	private final Set<Staff> employees = new HashSet<>();
+
+	public void hire(final Staff employee) {
+		employees.add(employee);
+	}
+
+	public void fire(final Staff employee) {
+		employees.remove(employee);
+	}
+
+	@Override
+	public Iterator<Staff> iterator() {
+		return employees.iterator();
+	}
+}
